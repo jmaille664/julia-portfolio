773cda1c2fef30e95750be675054ad451671c64f
jmaille664 <juliamaille@gmail.com>
2022-05-21 16:16:32 -0400

    added alt tags for images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ export default function Projects(){
         {/* project 1 */}
         <section className= "project-row">
         <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
-          <img className="project-img" src={ZakPortfolio}></img>
+          <img className="project-img" src={ZakPortfolio} alt="Zak Rittenhouse Portfolio Project"></img>
           </a>
         <h3 className="projects-3">Zak Rittenhouse Portfolio</h3>
         <p className="projects-paragraph">Group Capstone project during my coding bootcamp. Zak wanted a website to showcase his professional learning development work. A critical aspect of the project was to ensure accessibility to all user. Zak also wanted the ability for users to contact him.</p>
@@ -25,15 +25,15 @@ export default function Projects(){
   </section> 
   {/* project 2 */}
   <section className= "project-row">
-        <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
-          <img className="project-img" src={WavePortal}></img>
+        <a href="https://wave-portal-ashy.vercel.app/" target="_blank">
+          <img className="project-img" src={WavePortal} alt="Wave Portal Project"></img>
           </a>
           <br></br>
           <br></br>
         <h3 className="projects-3">Web3 Wave Portal</h3>
         <p className="projects-paragraph">Wrote and deployed a smart contract to the blockchain. Wave Portal allows users to send a wave and message and the data is save on the blockchain through an Ethereum smart contract. Website allows people to connect their wallets and interact with the smart contract. </p>
         <p className="projects-paragraph">Technologies Used: JavaScript, React, Solidity </p>
-        <a href="https://github.com/Interactive-Portfolio-for-Zak/capstone-project" target="_blank">
+        <a href="https://github.com/jmaille664/wave-portal" target="_blank">
     <button className="button1">Project Github</button>
   </a> 
   <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
