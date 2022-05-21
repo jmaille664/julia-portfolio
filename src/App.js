786b2e2a36ed14efd64373d26ba6fc79c611ff33
jmaille664 <juliamaille@gmail.com>
2022-05-21 15:44:49 -0400

    Added two projects to project section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Card, Button} from "react-bootstrap";
 import './App.css';
 import Home from "./components/Home.js"
 import JM from "./image/JM.png"
-import NavBar from "./components/Home.js"
 import About from "./components/About.js"
 import Skills from "./components/Skills.js"
 import Footer from "./components/Footer.js"
 import Projects from "./components/Projects.js"
+ 
 
 
 function App() {
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,46 @@
 import React from 'react'
-import {Card, Button} from "react-bootstrap";
 import ZakPortfolio from "../image/Zak-Portfolio.JPG"
+import WavePortal from "../image/wave-project.JPG"
 
 export default function Projects(){
 
     return <>
-    <section className="projects">
+    <div className="projects">
+    <h2 className="projects-h2"> Projects</h2>
     <div className="projects-content">
-        <h2 className="projects-h2"> Projects</h2>
-        {/* <Card style={{ width: '1rem' }}>
-  <Card.Img variant="top" src={ZakPortfolio}/>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card> */}
-        </div>
-    </section>
+        {/* project 1 */}
+        <section className= "project-row">
+        <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
+          <img className="project-img" src={ZakPortfolio}></img>
+          </a>
+        <h3 className="projects-3">Zak Rittenhouse Portfolio</h3>
+        <p className="projects-paragraph">Group Capstone project during my coding bootcamp. Zak wanted a website to showcase his professional learning development work. A critical aspect of the project was to ensure accessibility to all user. Zak also wanted the ability for users to contact him.</p>
+        <p className="projects-paragraph">Technologies Used: MongoDB, Express.js, Nodemailer, JavaScript, React, ReactBoostrap, Node.js </p>
+        <a href="https://github.com/Interactive-Portfolio-for-Zak/capstone-project" target="_blank">
+    <button className="button1">Project Github</button>
+  </a> 
+  <a href="https://zak-portfolio-capstone.herokuapp.com/">
+    <button className="button2">Live Site</button>
+  </a>
+  </section> 
+  {/* project 2 */}
+  <section className= "project-row">
+        <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
+          <img className="project-img" src={WavePortal}></img>
+          </a>
+          <br></br>
+          <br></br>
+        <h3 className="projects-3">Web3 Wave Portal</h3>
+        <p className="projects-paragraph">Wrote and deployed a smart contract to the blockchain. Wave Portal allows users to send a wave and message and the data is save on the blockchain through an Ethereum smart contract. Website allows people to connect their wallets and interact with the smart contract. </p>
+        <p className="projects-paragraph">Technologies Used: JavaScript, React, Solidity </p>
+        <a href="https://github.com/Interactive-Portfolio-for-Zak/capstone-project" target="_blank">
+    <button className="button1">Project Github</button>
+  </a> 
+  <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
+    <button className="button2">Live Site</button>
+  </a> 
+        </section>
+       </div> 
+    </div>
     </>
 }
