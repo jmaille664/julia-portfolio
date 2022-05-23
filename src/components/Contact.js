b9e34c7f073d19f41778682b00b751ce4b19d6f9
jmaille664 <juliamaille@gmail.com>
2022-05-22 20:45:51 -0400

    updated responsiveness on contact section and added LinkedIn link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,13 @@ export default function Contact(){
     <div className="contact">
     <h2 className="contact-h2">Contact Me</h2>
     <div className="contact-content"></div>
-    <p className="contact-content-p">Freelance Work? Accessibility Consulting? Prospective Employement? Feel free to reach out using my social links if you want to work with me, have questions, or just want to connect!</p><br/><br/>
+    <p className="contact-content-p">Freelance Work? Accessibility Consulting? Prospective Employement? Feel free to reach out using my social links below if you want to work with me, have questions, or just want to connect!</p><br/>
+    <a
+            /*Linkedin image and website link */
+              className="contact-linkedin"
+              href="https://www.linkedin.com/in/JuliaMaille"
+              target="_blank"
+            >Connect with me on LinkedIn here</a>
     </div>
     </>
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ZakPortfolio from "../image/Zak-Portfolio.JPG"
 import WavePortal from "../image/wave-project.JPG"
+import ZakPortfolio2 from "../image/ZakPortfolio2.JPG"
 
 export default function Projects(){
 
@@ -11,16 +12,16 @@ export default function Projects(){
         {/* project 1 */}
         <section className= "project-row">
         <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
-          <img className="project-img" src={ZakPortfolio} alt="Zak Rittenhouse Portfolio Project"></img>
+          <img className="project-img" src={ZakPortfolio2} alt="Capstone Project"></img>
           </a>
-        <h3 className="projects-3">Zak Rittenhouse Portfolio</h3>
-        <p className="projects-paragraph">Group Capstone project during my coding bootcamp. Zak wanted a website to showcase his professional learning development work. A critical aspect of the project was to ensure accessibility to all user. Zak also wanted the ability for users to contact him.</p>
+        <h3 className="projects-3">Capstone Project</h3>
+        <p className="projects-paragraph">This was my final project during my coding bootcamp, I worked in a group to build a personal portfoli webstie for a client. I focused on website accessibility, back-end server, database and form connection, and front end development.</p>
         <p className="projects-paragraph">Technologies Used: MongoDB, Express.js, Nodemailer, JavaScript, React, ReactBoostrap, Node.js </p>
         <a href="https://github.com/Interactive-Portfolio-for-Zak/capstone-project" target="_blank">
-    <button className="button1">Project Github</button>
+    <button className="button1">View Capstone Project On Github</button>
   </a> 
   <a href="https://zak-portfolio-capstone.herokuapp.com/">
-    <button className="button2">Live Site</button>
+    <button className="button2">View Capstone Project Website</button>
   </a>
   </section> 
   {/* project 2 */}
@@ -31,13 +32,13 @@ export default function Projects(){
           <br></br>
           <br></br>
         <h3 className="projects-3">Web3 Wave Portal</h3>
-        <p className="projects-paragraph">Wrote and deployed a smart contract to the blockchain. Wave Portal allows users to send a wave and message and the data is save on the blockchain through an Ethereum smart contract. Website allows people to connect their wallets and interact with the smart contract. </p>
+        <p className="projects-paragraph">In this project I wrote and deployed a smart contract to the blockchain. This project allows users to send a message and the data is save on the blockchain through an Ethereum smart contract. </p>
         <p className="projects-paragraph">Technologies Used: JavaScript, React, Solidity </p>
         <a href="https://github.com/jmaille664/wave-portal" target="_blank">
-    <button className="button1">Project Github</button>
+    <button className="button1">View Wave Portal Project On Github</button>
   </a> 
   <a href="https://zak-portfolio-capstone.herokuapp.com/" target="_blank">
-    <button className="button2">Live Site</button>
+    <button className="button2">View Wave Portal Project Website</button>
   </a> 
         </section>
      
